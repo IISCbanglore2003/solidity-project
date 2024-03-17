28f6ef8aa8b4e8fa168c84045fece37876ea0404
IISCbanglore2003 <163749377+IISCbanglore2003@users.noreply.github.com>
2024-03-17 21:13:28 +0530

    Update and rename Project 1 to Project1.sol

--- Project1.sol
+++ Project1.sol
@@ -3,21 +3,21 @@ pragma solidity 0.8.18;
 
 contract MyToken {
    
-  // public variables here
+  // public variables below
     string public tokenName = "META";
     string public tokenAbbry = "HTA";
     uint public totalSupply = 0;
     
- // mapping variable here
+ // mapping variable below
     mapping(address => uint) public balances;
      
-// mint function
+// mint function below
     function mint (address _address, uint _value) public {
        totalSupply += _value;
        balances[_address] += _value;
     }
     
-// burn function
+// burn function below
     function burn (address _address, uint _value) public {
        if (balances[_address] >= _value) {
           totalSupply -= _value;
